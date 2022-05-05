1de54d9c51539201f84f9f0649b8ca591b466137
Moonlight <2324154234@qq.com>
2022-05-05 10:12:56 +0800

    test

---
+++ src/main/java/com/example/feiyang/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.feiyang.service;
+
+/**
+ * @author 望舒
+ * @date 2022/5/5 10:12
+ */
+
+public class UserService {
+    public void test() {
+        System.out.println(111);
+    }
+}
