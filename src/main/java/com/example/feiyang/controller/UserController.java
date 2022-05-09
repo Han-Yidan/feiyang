9ccec99b59b63ad386db1373dbf3a472a36954db
hyd <75252063+Han-Yidan@users.noreply.github.com>
2022-05-09 12:53:35 +0800

    管理员模块： 管理员设置技术员

--- src/main/java/com/example/feiyang/controller/UserController.java
+++ src/main/java/com/example/feiyang/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.example.feiyang.controller;
 
 import com.example.feiyang.common.utils.JsonResponse;
-import com.example.feiyang.service.serviceImpl.UserServiceImpl;
+import com.example.feiyang.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/example/feiyang/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/feiyang/service/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.feiyang.service.serviceImpl;
+package com.example.feiyang.service.impl;
 
 import com.example.feiyang.common.utils.JsonResponse;
 import com.example.feiyang.common.utils.MessageUtils;
@@ -7,17 +7,9 @@ import com.example.feiyang.dao.UserMapper;
 import com.example.feiyang.entity.User;
 import com.example.feiyang.entity.UserExample;
 import com.example.feiyang.service.UserService;
-import com.tencentcloudapi.common.Credential;
-import com.tencentcloudapi.common.exception.TencentCloudSDKException;
-import com.tencentcloudapi.common.profile.ClientProfile;
-import com.tencentcloudapi.common.profile.HttpProfile;
-import com.tencentcloudapi.sms.v20210111.SmsClient;
-import com.tencentcloudapi.sms.v20210111.models.SendSmsRequest;
-import com.tencentcloudapi.sms.v20210111.models.SendSmsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.sound.midi.Soundbank;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
