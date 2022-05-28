b91afc6d737dfbd67e6991846064df4604c3113e
hyd <75252063+Han-Yidan@users.noreply.github.com>
2022-05-29 00:03:22 +0800

    管理员用户注册接口

--- src/main/java/com/example/feiyang/controller/AdminController.java
+++ src/main/java/com/example/feiyang/controller/AdminController.java
@@ -8,10 +8,7 @@ import com.example.feiyang.service.impl.FeedServiceImpl;
 import com.example.feiyang.service.impl.QuestionServiceImpl;
 import com.example.feiyang.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -53,5 +50,17 @@ public class AdminController extends BaseController {
         res.put("TotalAdmin", adminService.getTotalAdmin());
 
         return JsonResponse.success(res, "获取成功！");
+    }
+
+    /**
+     * 管理员用户注册
+     *
+     * @param params
+     * @return
+     */
+    @RequestMapping("reg")
+    public JsonResponse<Void> reg(@RequestBody Map<String, Object> params) {
+        adminService.reg(params);
+        return new JsonResponse<>(OK);
     }
 }
--- src/main/java/com/example/feiyang/service/AdminService.java
+++ src/main/java/com/example/feiyang/service/AdminService.java
@@ -3,9 +3,13 @@ package com.example.feiyang.service;
 
 import com.example.feiyang.common.utils.JsonResponse;
 
+import java.util.Map;
+
 
 public interface AdminService {
     JsonResponse login(String username, String password);
+
+    void reg(Map<String, Object> params);
 
     //返回所有管理员总数
     Integer getTotalAdmin();
