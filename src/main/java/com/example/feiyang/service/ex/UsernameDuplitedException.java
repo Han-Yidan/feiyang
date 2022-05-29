807ac109a5fb516ea1ca04b060cd76abc867c76a
hyd <75252063+Han-Yidan@users.noreply.github.com>
2022-05-29 16:12:49 +0800

    1

---
+++ src/main/java/com/example/feiyang/service/ex/UsernameDuplitedException.java
@@ -0,0 +1,26 @@
+package com.example.feiyang.service.ex;
+
+/**
+ * 用户名被占用的异常
+ **/
+public class UsernameDuplitedException extends ServiceException {
+    public UsernameDuplitedException() {
+        super();
+    }
+
+    public UsernameDuplitedException(String message) {
+        super(message);
+    }
+
+    public UsernameDuplitedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UsernameDuplitedException(Throwable cause) {
+        super(cause);
+    }
+
+    protected UsernameDuplitedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
